5eded92a838a246ec1978d8552bd6df231abf5a1
Nick Verlinden <nick@studiokitara.com>
2017-07-04 17:04:58 +0200

    Initial commit.

---
+++ file-system.js
@@ -0,0 +1,45 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+//
+// allume.request.file-system
+//
+//    PKX request module for fetching releases from file system locations.
+//
+// License
+//    Apache License Version 2.0
+//
+// Copyright Nick Verlinden (info@createconform.com)
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+(function() {
+    var REQUEST_PROC_NAME = "file-system";
+    var URI_PATH_FILESYSTEM_TEMPLATE = "$NAME/";
+
+    function PKXRequestGitHub() {
+        var self = this;
+
+        this.process = function(selector) {
+            if (selector.uri.scheme != "file") {
+                return;
+            }
+
+            return new Promise(function (resolve, reject) {
+                selector.uri = selector.repository + URI_PATH_FILESYSTEM_TEMPLATE;
+                resolve();
+            });
+        };
+
+        // register request processor
+        define.Loader.waitFor("pkx", function(loader) {
+            loader.addRequestProcessor(REQUEST_PROC_NAME, self.process);
+        });
+    }
+
+    var processor = new PKXRequestGitHub();
+    define(function () {
+        return processor;
+    });
+
+    var version = require("./cc.version");
+    var string = require("./cc.string");
+})();
